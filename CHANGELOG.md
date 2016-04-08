dee9bcfa86efe838f293fc807f0890a40fb15e3e
Tim Wagner <t.wagner@techdivision.com>
2016-04-08 13:26:16 +0200

    Add functionality to support application based virtual host configurations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+# Version 2.0.0
+
+## Bugfixes
+
+* Add functionality to support application based virtual host configurations
+
+## Features
+
+* None
+
 # Version 1.0.0
 
 ## Bugfixes
--- src/AppserverIo/SingleApp/Core/Api/SimpleDeploymentService.php
+++ src/AppserverIo/SingleApp/Core/Api/SimpleDeploymentService.php
@@ -20,11 +20,8 @@
 
 namespace AppserverIo\SingleApp\Core\Api;
 
-use AppserverIo\Configuration\ConfigurationException;
-use AppserverIo\Appserver\Core\Api\Node\ContextNode;
+use AppserverIo\Properties\PropertiesInterface;
 use AppserverIo\Appserver\Core\Api\DeploymentService;
-use AppserverIo\Appserver\Core\Api\Node\ContainersNode;
-use AppserverIo\Appserver\Core\Api\Node\DeploymentNode;
 use AppserverIo\Appserver\Core\Utilities\SystemPropertyKeys;
 use AppserverIo\Appserver\Core\Interfaces\ContainerInterface;
 
@@ -39,6 +36,24 @@ use AppserverIo\Appserver\Core\Interfaces\ContainerInterface;
  */
 class SimpleDeploymentService extends DeploymentService
 {
+
+    /**
+     * Prepare's the system properties for the actual mode, which is the runner mode in our case.
+     *
+     * @param \AppserverIo\Properties\PropertiesInterface $properties The properties to prepare
+     * @param string                                      $webappPath The path of the web application to prepare the properties with
+     *
+     * @return void
+     */
+    protected function prepareSystemProperties(PropertiesInterface $properties, $webappPath)
+    {
+
+        // let the parent method also prepare the properties
+        parent::prepareSystemProperties($properties, $webappPath);
+
+        // replace the host's application base directory with the parent directory
+        $properties->add(SystemPropertyKeys::HOST_APP_BASE, dirname($webappPath));
+    }
 
     /**
      * Loads the containers, defined by the applications, merges them into
@@ -64,56 +79,7 @@ class SimpleDeploymentService extends DeploymentService
 
         /** @var AppserverIo\Appserver\Core\Api\Node\ContainerNodeInterface $containerNodeInstance */
         foreach ($systemConfiguration->getContainers() as $containerNode) {
-
-            // our webapp path is the actual working directory
-            $webappPath = getcwd();
-
-            // iterate through all server configurations (servers.xml), validate and merge them
-            foreach ($this->globDir($webappPath . '/META-INF/containers.xml') as $containersConfigurationFile) {
-                try {
-                    // validate the application specific container configurations
-                    $configurationService->validateFile($containersConfigurationFile, null);
-
-                    // create a new containers node instance
-                    $containersNodeInstance = new ContainersNode();
-                    $containersNodeInstance->initFromFile($containersConfigurationFile);
-
-                    // load the system properties
-                    $properties = $this->getSystemProperties($containerNode);
-
-                    // replace the host's application base directory with the parent directory
-                    $properties->add(SystemPropertyKeys::HOST_APP_BASE, dirname($webappPath));
-
-                    // append the application specific properties and replace the properties
-                    $properties->add(SystemPropertyKeys::WEBAPP, $webappPath);
-                    $properties->add(SystemPropertyKeys::WEBAPP_NAME, basename($webappPath));
-
-                    /** @var AppserverIo\Appserver\Core\Api\Node\ContainerNodeInterface $containerNodeInstance */
-                    foreach ($containersNodeInstance->getContainers() as $containerNodeInstance) {
-                        // replace the properties for the found container node instance
-                        $containerNodeInstance->replaceProperties($properties);
-                        // query whether we've to merge or append the server node instance
-                        if ($container = $systemConfiguration->getContainer($containerNodeInstance->getName())) {
-                            $container->merge($containerNodeInstance);
-                        } else {
-                            $systemConfiguration->attachContainer($containerNodeInstance);
-                        }
-                    }
-
-                } catch (ConfigurationException $ce) {
-                    // load the logger and log the XML validation errors
-                    $systemLogger = $this->getInitialContext()->getSystemLogger();
-                    $systemLogger->error($ce->__toString());
-
-                    // additionally log a message that server configuration will be missing
-                    $systemLogger->critical(
-                        sprintf(
-                            'Will skip app specific server configuration because of invalid file %s',
-                            $containersConfigurationFile
-                        )
-                    );
-                }
-            }
+            $this->loadContainerInstance($containerNode, $systemConfiguration, getcwd());
         }
 
         // returns the merged system configuration
